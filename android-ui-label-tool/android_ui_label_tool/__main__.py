90bbae1cccd6758d9ea2d6bebda3f723faa561bf
jackson <jackson.kubin@gmail.com>
2024-09-03 10:32:42 -0400

    add xml parsing

--- android-ui-label-tool/android_ui_label_tool/__main__.py
+++ android-ui-label-tool/android_ui_label_tool/__main__.py
@@ -1,10 +1,33 @@
-import argparse
 from pathlib import Path
 from tap import Tap
+import xml.etree.ElementTree as ET 
+
+class Args(Tap):
+    file_path: Path = Path.cwd()
+
+def find_leaves(node: ET.Element):
+    children = [*node.findall('*')]
+    if len(children) == 0:
+        return [node]
+    leaves: list[ET.Element] = []
+    for child in children:
+        leaves.extend(find_leaves(child))
+    return leaves
+
+
+def process_xml(xml_file: Path):
+    png_file = xml_file.with_suffix(".png")
+    if not png_file.exists():
+        raise ValueError(f"Corresponding PNG file {png_file} does not exist")
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    leaves = find_leaves(root)
+    for leaf in leaves:
+        print(leaf.tag, leaf.get('bounds'))
+    
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("file_path", type=Path)
+    parser = Args()
 
     p = parser.parse_args()
     if not (p.file_path.is_dir() and p.file_path.exists()):
@@ -12,6 +35,10 @@ if __name__ == "__main__":
 
     xml_files = p.file_path.glob("*.xml")
     for filename in xml_files:
-        print(filename)
+        xml_file = p.file_path / filename
+        try:
+            process_xml(xml_file)
+        except Exception as e:
+            print(f"Error processing {xml_file}: {e}")
 
     
